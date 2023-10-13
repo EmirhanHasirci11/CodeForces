8b09288daa2a4859a345b1186e8ab0d131d100bc
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-13 23:46:22 +0300

    231A-Team

---
+++ 231A-Team/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace Team
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int countOfSolution = Convert.ToInt32(Console.ReadLine());            
+            for(int i=0;i<countOfSolution;i++)
+            {
+               
+                int votesSum=Console.ReadLine().Trim().Split(' ').Select(x=>int.Parse(x)).Sum();
+                if (votesSum >= 2)
+                    result++;
+
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
