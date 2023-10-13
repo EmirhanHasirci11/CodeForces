a725d96c9d72d470b27e8f9ad79f6f91f9a05d48
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-14 00:06:38 +0300

    228A-IsYourHorsesoeontheotherhoof

---
+++ 228A-IsYourHorsesoeontheotherhoof/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace IsYourHorsesoeontheotherhoof
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            Dictionary<string, int> map = new();
+            string[] input = Console.ReadLine().Trim().Split(' ').ToArray();
+            foreach (string val in input)            
+                if (!map.TryAdd(val, 1))
+                    map[val]++;
+            foreach (string val in map.Keys)
+                if (map[val] > 1)
+                    result += map[val] - 1;
+            Console.WriteLine(result);
+
+        }
+    }
+}
