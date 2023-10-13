c48f9845a2f70ac91249d7ec00ee3fb2020db51a
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-14 00:40:49 +0300

    118A-StringTask

---
+++ 118A-StringTask/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace StringTask
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            char[] vowels = new char[] {'a','o','y','e','u','i' };
+            string input = Console.ReadLine().ToLower();
+            StringBuilder sb = new();
+            for(int i=0;i<input.Length;i++)
+            {
+                if (!vowels.Contains(input[i]))
+                {
+                    sb.Append(".");
+                    sb.Append(input[i]);
+                }
+            }
+            Console.WriteLine(sb.ToString());
+            
+        }
+        
+    }
+}
