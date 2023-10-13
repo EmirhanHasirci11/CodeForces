5a3926f5307d6401606ed2c21705fa20876fed46
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-14 00:16:46 +0300

    236A-BoyOrGirl

---
+++ 236A-BoyOrGirl/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace BoyorGirl
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            int CountOfDistinct = Console.ReadLine().Distinct().Count();
+            Console.WriteLine(CountOfDistinct % 2 == 0 ? "CHAT WITH HER!" : "IGNORE HIM!");
+        }
+    }
+}
